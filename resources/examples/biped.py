286facfbeaf37967330fa5006c774dae28744c8f
moriah.tobin <moriah.tobin@16731396-06f5-0310-8873-f7f720988828>
2013-02-20 22:28:36 +0000

    Now manages DOM 77,7 and it's poor Simulated Timing

--- resources/examples/biped.py
+++ resources/examples/biped.py
@@ -15,14 +15,35 @@ if len(sys.argv) < 3:
 files = sys.argv[2:]
 
 muon_service = photonics_service.I3PhotoSplineService('/net/user/mntobin/IceRec/Tables/emu_abs150.fits', '/net/user/mntobin/IceRec/Tables/emu_prob150.fits', 0)
-cascade_service = photonics_service.I3PhotoSplineService('/net/user/mntobin/IceRec/Tables/ems_spice1_z20_a10_150.abs.fits', '/net/user/mntobin/IceRec/Tables/ems_spice1_z20_a10_150.prob.fits', 0)
+#cascade_service = photonics_service.I3PhotoSplineService('/net/user/mntobin/IceRec/Tables/ems_spice1_z20_a10_150.abs.fits', '/net/user/mntobin/IceRec/Tables/ems_spice1_z20_a10_150.prob.fits', 0)
 cascade_service_mie = photonics_service.I3PhotoSplineService('/net/user/mntobin/IceRec/Tables/ems_mie_z20_a10_150.abs.fits', '/net/user/mntobin/IceRec/Tables/ems_mie_z20_a10_150.prob.fits', 0)
 
 tray = I3Tray()
 tray.AddModule('I3Reader', 'reader', FilenameList=files)
+#fixup code from JVS
+def flag_borked_slc(frame):
+        # Find SLC launches affected by DOMsimulator off-by-one bug,
+        # and tell Millipede to ignore them
+        bad_keys = set()
+        import numpy
+        for om, dls in frame['InIceRawData'].iteritems():
+                for dl in dls:
+                        if dl.lc_bit == False and dl.charge_stamp_highest_sample == 0:
+                                print '%s, she is the bad!' % om
+                                bad_keys.add(om)
+        if len(bad_keys) > 0:
+                frame['BorkedSLC'] = dataclasses.I3VectorOMKey(bad_keys)
+                frame['OfflinePulses_NoBorkedSLC'] = dataclasses.I3RecoPulseSeriesMapMask(frame, 'OfflinePulses',
+                     lambda om, idx, pulse: om not in bad_keys)
+        else:
+                frame['OfflinePulses_NoBorkedSLC'] = dataclasses.I3RecoPulseSeriesMapMask(frame, 'OfflinePulses')
+tray.AddModule(flag_borked_slc, 'slc_filter', Streams=[icetray.I3Frame.DAQ])
+
 tray.AddModule("I3WaveformTimeRangeCalculator","WaveformTimeRange")
 tray.AddModule(wavedeform.AddMissingTimeWindow,"PulseTimeRange",Streams=[icetray.I3Frame.DAQ])
-
+def copytimerange(frame):
+	frame['OfflinePulses_NoBorkedSLCTimeRange']=frame['OfflinePulsesTimeRange']
+tray.AddModule(copytimerange, "CopyTimeRange")
 
 tray.AddService('BipedParametrizationFactory', 'bipedparam',
     MuonSpacing=1, ShowerSpacing=1, StepT=0, StepX=2, StepY=2, StepZ=2,    
@@ -32,7 +53,7 @@ tray.AddService('BipedParametrizationFactory', 'bipedparam',
 )
 tray.AddService('BipedLikelihoodFactory', 'bipedllh',
     MuonPhotonicsService=muon_service, CascadePhotonicsService=cascade_service_mie,
-    PhotonsPerBin=1, Pulses='OfflinePulses')
+    PhotonsPerBin=1, Pulses='OfflinePulses_NoBorkedSLC')
 tray.AddService('I3GSLRandomServiceFactory','I3RandomService')
 tray.AddService('I3GulliverMinuit2Factory', 'minuit',
     MaxIterations=2000, Algorithm='SIMPLEX')
