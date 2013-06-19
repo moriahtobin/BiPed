1871f163da0f362a0cd44cf6d708a25a6b791d27
moriah.tobin <moriah.tobin@16731396-06f5-0310-8873-f7f720988828>
2013-06-19 22:43:13 +0000

    nan llh calls

--- private/LikBiped.cxx
+++ private/LikBiped.cxx
@@ -14,6 +14,8 @@ class BipedLikelihood : public I3MillipedeService,
 	public:
 		BipedLikelihood(const I3Context &context);
 
+		void Configure();
+		double muonspacing_;
 		// Gulliver interface
 		void SetEvent(const I3Frame &);
                 void SetGeometry(const I3Geometry &geo) {};
@@ -34,6 +36,7 @@ class BipedLikelihood : public I3MillipedeService,
 			
 	private:
 		SET_LOGGER("BipedLikelihood");
+
 		
 		I3VectorI3ParticlePtr ExtractHypothesis(const I3EventHypothesis&);
 };
@@ -41,7 +44,12 @@ class BipedLikelihood : public I3MillipedeService,
 I3_POINTER_TYPEDEFS(BipedLikelihood);
 
 BipedLikelihood::BipedLikelihood(const I3Context &context) : I3MillipedeService(context), I3EventLogLikelihoodBase()
-{}
+{
+	AddParameter("MuonSpacing", "Spacing of muon (ionization) sources "
+	    "along the track, in meters. MUST match source extension in "
+	    "muon tables. Set to 0 to not include ionization sources.",
+	    1*I3Units::m);
+}
 
 unsigned int
 BipedLikelihood::GetMultiplicity()
@@ -65,6 +73,7 @@ double
 BipedLikelihood::GetLogLikelihood(const I3EventHypothesis &hypo)
 {
 	return GetLogLikelihood(hypo, NULL, false, 1.);
+	//log_info("This is where we're suppose to define the HYPOTHESIS");
 }
 
 double
@@ -72,6 +81,7 @@ BipedLikelihood::GetLogLikelihoodWithGradient(const I3EventHypothesis &hypo,
     I3EventHypothesis &gradient, double weight)
 {
 	return GetLogLikelihood(hypo, &gradient, true, 1.);
+	//log_info("This is where we're suppose to define the GRADIENT");
 }
 
 I3VectorI3ParticlePtr
@@ -84,6 +94,7 @@ BipedLikelihood::ExtractHypothesis(const I3EventHypothesis &hypo)
 		sources->push_back(*hypo.particle);
 	}
 	return sources;
+	//log_info("We Have Sources!");
 }
 
 I3FrameObjectPtr
@@ -103,48 +114,129 @@ BipedLikelihood::GetDiagnostics(const I3EventHypothesis &hypo)
 		    response_matrix, params.get(), &c);
 	cholmod_l_free_sparse(&response_matrix, &c);
 	
+	//log_info("We Have Parameters!");
 	return params;
+}
+
+void
+BipedLikelihood::Configure()
+{
+	I3MillipedeService::Configure();
+
+	GetParameter("MuonSpacing", muonspacing_);
+	//log_info("%f is the muon spacing in LikConfig", muonspacing_); 
 }
 
 double
 BipedLikelihood::GetLogLikelihood(const I3EventHypothesis &hypo,
     I3EventHypothesis *gradient, bool fit_energy, double weight)
 {
+	//log_info("%f is the muon spacing", muonspacing_); 
 	cholmod_sparse *response_matrix, *gradients;
 	double llh = 0.;
 
 	I3VectorI3ParticlePtr sources = ExtractHypothesis(hypo);
+
 
 	I3VectorI3ParticlePtr gradsources;
 	if (gradient != NULL) {
 		gradsources = ExtractHypothesis(*gradient);
 		assert(sources->size() == gradsources->size());
 	}
+// Make a vector of particles out of the cascade & muon:
+	// cascade is the starting cascade, and muon is replaced
+	// by equal energy, closely spaced cascades to simulate
+	// min ionizing. 
 	
-	response_matrix = Millipede::GetResponseMatrix(domCache_, *sources,
+	// Start with cascade as opening particle:
+	boost::shared_ptr<I3Vector<I3Particle> > microSources(new I3Vector<I3Particle>);
+	microSources->push_back((*sources)[0]);
+	//log_info("Pushing back Sources");
+
+	// get total track length from hypothesis muon
+	double trackLength = (*sources)[1].GetLength();
+	double zen_cascade = (*sources)[0].GetZenith();
+	double zen_track = (*sources)[1].GetZenith();
+	double azi_track = (*sources)[1].GetAzimuth();
+	double En_casc = (*sources)[0].GetEnergy();
+	double x_v = (*sources)[0].GetX();
+	double y_v = (*sources)[0].GetY();
+	double z_v = (*sources)[0].GetZ();
+
+	// space cascades closely to simulate min ionizing muon
+	I3Particle PrevParticle = (*sources)[1]; //start with hypothsis muon
+	//log_info("%d is the particle type of PrevParticle for Muon Looping", PrevParticle.GetType());
+	//log_info("Start Yo Particles");
+	I3Particle NextParticle = PrevParticle;
+	//log_info("There's Some Sweet LoOping Happening Here");
+	for (double d=0; d<trackLength ; d = d+muonspacing_){
+		//log_info("in zeh 4 loopz");
+		//log_info("%f is the length of our Muon now", d);
+		//log_info("%f is the total length of our muon", trackLength);
+		//log_info("%f is the muon spacing", muonspacing_); 
+		NextParticle.SetPos(PrevParticle.ShiftAlongTrack(muonspacing_));
+		//log_info("Where is my next particle T.T");
+		microSources->push_back(NextParticle);
+		//log_info("Push that Particle Back");
+		PrevParticle = NextParticle;
+	}
+	log_info("We have finished defining Muon test points");
+
+	// Make a matrix of ones and zeros for collapsing stuff
+	// (a triplet is: matrix position i,j; and value x)
+	cholmod_triplet *pen_trip = cholmod_l_allocate_triplet(
+	    (*microSources).size(), (*sources).size(), (*microSources).size(), 0,
+	    CHOLMOD_REAL, &c);
+	//log_info("TRIPLETS");
+	// starting number of non-zero entries in this matrix:
+	pen_trip->nnz = 0;
+	//log_info("we be trippin'...on pens");
+	for (unsigned i = 0; i < pen_trip->nrow; i++) {
+		((long *)(pen_trip->i))[pen_trip->nnz] = i;
+		((long *)(pen_trip->j))[pen_trip->nnz] = (i != 0);
+		((double *)(pen_trip->x))[pen_trip->nnz] = 1;
+		pen_trip->nnz++;
+	}
+	//log_info("Done with Pen Tripping LoOp");
+	cholmod_sparse *collapser = cholmod_l_triplet_to_sparse(pen_trip, 0, &c);
+	//log_info("The Collapser");
+	cholmod_l_free_triplet(&pen_trip, &c);
+	//log_info("Free the Triplets from the pens");
+	
+	response_matrix = Millipede::GetResponseMatrix(domCache_, *microSources,
 	    domEfficiency_, muon_p, cascade_p,
 	    (gradient == NULL) ? NULL : &gradients, &c);
+	//log_info("Response_matrix has been defined");
 	if (response_matrix == NULL)
 		log_fatal("Null basis matrix");
+
+	// Colapse the resulting big matrix back into a 2-particle matrix
+	cholmod_sparse little_response_matrix = *cholmod_l_ssmult (response_matrix, 
+	    collapser, CHOLMOD_REAL, 1, 0, &c) ;
+	//log_info("Guess What's delicious guys? Little Matrices");
 	
 	if (fit_energy) {
-		SolveEnergyLosses(*sources, response_matrix,
+		SolveEnergyLosses(*microSources, &little_response_matrix,
 		    (gradient == NULL ? NULL : gradients));
 		if (sources->size() == 1)
 			hypo.particle->SetEnergy((*sources)[0].GetEnergy());
 	}
+	//log_info("fit for cascade energy");
 	
-	llh = Millipede::FitStatistics(domCache_, *sources, I3Units::MeV,
+	llh = Millipede::FitStatistics(domCache_, *microSources, I3Units::MeV,
 	    response_matrix, NULL, &c);
+		log_info("[%f m track, (%f zen mu, %f zen cascade), vertex (%f, %f, %f), E_c %f, azi %f] -> (llh=%f)", trackLength, zen_track, zen_cascade, x_v, y_v, z_v, En_casc, azi_track, llh);
 
 	if (gradient != NULL) {
 		Millipede::LLHGradient(domCache_, *sources, *gradsources,
-		    I3Units::MeV, weight, response_matrix, gradients, &c);
+		    I3Units::MeV, weight, &little_response_matrix, gradients, &c);
 		cholmod_l_free_sparse(&gradients, &c);
+		//log_info("We are in the gradient statement");
 		if (sources->size() == 1) {
 			// NB: the requested weight is already applied in
 			// the call to LLHGradient()
 			I3Particle &p = *gradient->particle;
+			//log_info("Define p");
 			p.SetPos(I3Position(
 			    p.GetPos().GetX() +
 			      (*gradsources)[0].GetPos().GetX(),
@@ -153,18 +245,24 @@ BipedLikelihood::GetLogLikelihood(const I3EventHypothesis &hypo,
 			    p.GetPos().GetZ() +
 			      (*gradsources)[0].GetPos().GetZ()
 			));
+			//log_info("Increased Position");
 			p.SetTime(p.GetTime() + (*gradsources)[0].GetTime());
+			//log_info("Increased Time");
 			p.SetDir(I3Direction(
 			    p.GetDir().GetZenith() +
 			      (*gradsources)[0].GetDir().GetZenith(),
 			    p.GetDir().GetAzimuth() +
 			      (*gradsources)[0].GetDir().GetAzimuth()
-			));			
+			));
+			//log_info("Increased Zen and Azi");			
 		}
+	}
+	if (gradient == NULL) {
+	log_info("NULL GRADIENT MATRIX");
 	}
 
 	cholmod_l_free_sparse(&response_matrix, &c);
-	
+	//log_info("We just increased some values, so let's give 'em to the llh");
 	return llh;
 }
 
--- private/ParaBiped.cxx
+++ private/ParaBiped.cxx
@@ -9,8 +9,7 @@
 
 static void BipedHypothesis(I3ParticleConstPtr track,
     std::vector<I3Particle> &hypothesis, double boundary,
-    double muonspacing, double showerspacing,
-    double slantstart, double slantstop);
+    double muonspacing);
 
 
 
@@ -21,14 +20,12 @@ class BipedParametrization : public I3SimpleParametrization {
 
 		double boundary_;
 		double muonspacing_;
-		double showerspacing_;
 		std::string name_;
 		double starting_cascade_dirstep_;
-		double slantstart_;
-		double slantstop_;
 
 		void UpdatePhysicsVariables();
 		void UpdateParameters();
+		bool InitChainRule(bool grad); 
 		void ApplyChainRule();
 	private:
 		SET_LOGGER("BipedParametrization");
@@ -49,19 +46,10 @@ BipedParametrization::BipedParametrization(const I3Context& context)
 	AddParameter("MuonSpacing", "Spacing of muon (ionization) sources "
 	    "along the track, in meters. MUST match source extension in "
 	    "muon tables. Set to 0 to not include ionization sources.",
-	    15*I3Units::m);
-	AddParameter("ShowerSpacing", "Spacing of shower (radiative) "
-	    "sources along the track, in meters. Set to 0 to not include "
-	    "radiative losses.", 15*I3Units::m);
+	    1*I3Units::m);
 	AddParameter("StartingCascadeStepSize", "Allow the initial cascade to "
 	    "point in a different direction than the track, moving it in steps "
 	    "this size (starting direction fixed if 0).", 0);
-	AddParameter("StartSlantDepth", "Start of segment boundary in slant "
-	    "depth bins (fits segments in [Xstart,Xend] in detector "
-	    "coordinates). If negative, boundary used instead.", -1);
-	AddParameter("EndSlantDepth",  "End of Segment boundary in slant depth "
-	    "bins (fits segments in [Xstart,Xend] in detector coordinates). "
-	    "If negative, boundary used instead.", -1);
 }
 	
 void
@@ -74,9 +62,10 @@ BipedParametrization::UpdatePhysicsVariables()
 	boost::shared_ptr<I3Vector<I3Particle> > sources(new
 	    I3Vector<I3Particle>);
 	BipedHypothesis(hypothesis_->particle, *sources,
-	    boundary_, muonspacing_, showerspacing_, slantstart_, slantstop_);
+	    boundary_, muonspacing_);
+	//log_info("%f is the muon spacing in Para", muonspacing_); 
 
-	if (starting_cascade_dirstep_ > 0) {
+	if (starting_cascade_dirstep_ > 0 && sources->size() > 0) {
 		double p0 = par_[par_.size() - 2];
 		double p1 = par_[par_.size() - 1];
 		double seedX_, seedY_, seedZ_;
@@ -126,6 +115,17 @@ BipedParametrization::UpdateParameters()
 	UpdatePhysicsVariables();
 }
 
+bool 
+BipedParametrization::InitChainRule(bool want_grad) 
+{ 
+	I3SimpleParametrization::InitChainRule(want_grad); 
+
+	if (want_grad && starting_cascade_dirstep_ > 0) 
+		par_gradient_.resize(parspecs_.size()); 
+
+	return true; 
+} 
+
 void
 BipedParametrization::ApplyChainRule()
 {
@@ -173,6 +173,7 @@ BipedParametrization::ApplyChainRule()
 	I3SimpleParametrization::ApplyChainRule();
 
 	if (starting_cascade_dirstep_ > 0) {
+		//log_info("StartingCascadeDirStepPositive");
 		// Gradient for initial cascade angular difference to track
 		// The following code is mostly cribbed from the half-sphere
 		// parametrization
@@ -228,120 +229,49 @@ BipedParametrization::Configure()
 
 	GetParameter("Boundary", boundary_);
 	GetParameter("MuonSpacing", muonspacing_);
-	GetParameter("ShowerSpacing", showerspacing_);
 	GetParameter("StartingCascadeStepSize", starting_cascade_dirstep_);
-	GetParameter("StartSlantDepth", slantstart_);
-	GetParameter("EndSlantDepth", slantstop_);
 
 	if (starting_cascade_dirstep_ > 0) {
+		//log_info("OneMoreTimePositiveCascadeDirStep");
 		I3FitParameterInitSpecs specs("dir");
 		specs.minval_ = 0;
 		specs.maxval_ = 0;
 		specs.stepsize_ = starting_cascade_dirstep_;
 
 		specs.name_ = "cascadedir1";
+		//log_info("What is CascadeDir1?");
 		parspecs_.push_back(specs);
 		
 		specs.name_ = "cascadedir2";
 		parspecs_.push_back(specs);
+		//log_info("PushingBackTheSpecsForCascadeDir2");
 
 		par_.resize(parspecs_.size());
+		//log_info("Hath Thou Resized the Parspecs?");
 	}
+		//log_info("%f is the muon spacing AGAIN o.O", muonspacing_); 
 }
 
 static void
 BipedHypothesis(I3ParticleConstPtr track,
-    std::vector<I3Particle> &hypothesis, double boundary, double muonspacing,
-    double showerspacing, double slantstart, double slantstop)
+    std::vector<I3Particle> &hypothesis, double boundary, double muonspacing)
 {
-	double cross_times[6]; // x1, x2, y1, y2, z1, z2
-	double xspeed, yspeed, zspeed;
-	double entry_t, exit_t;
-
-	// Projections of particle speed on each axis
-	xspeed = track->GetSpeed()*sin(track->GetZenith() / I3Units::radian)*
-	    cos(track->GetAzimuth() / I3Units::radian);
-	yspeed = track->GetSpeed()*sin(track->GetZenith() / I3Units::radian)*
-	    sin(track->GetAzimuth() / I3Units::radian);
-	zspeed = track->GetSpeed()*cos(track->GetZenith() / I3Units::radian);
-
-	// Get the time points where it crosses the "detector" boundaries
-	// (defined as a cube of width 2*boundary_, centered at the detector
-	// origin)
-
-	cross_times[0] = track->GetTime() + (track->GetX() - boundary)/xspeed;
-	cross_times[1] = track->GetTime() + (track->GetX() + boundary)/xspeed;
-	cross_times[2] = track->GetTime() + (track->GetY() - boundary)/yspeed;
-	cross_times[3] = track->GetTime() + (track->GetY() + boundary)/yspeed;
-	cross_times[4] = track->GetTime() + (track->GetZ() - boundary)/zspeed;
-	cross_times[5] = track->GetTime() + (track->GetZ() + boundary)/zspeed;
-
-	// Sort the crossing points. The middle two are when it enters and
-	// exits the volume.
-	std::sort(&cross_times[0], &cross_times[6]);
-	entry_t = cross_times[2];
-	exit_t = cross_times[3];
-
-	if (track->GetShape() == I3Particle::StartingTrack) {
-		entry_t = track->GetTime();
-	} else if (track->GetShape() == I3Particle::StoppingTrack) {
-		exit_t = track->GetTime();
-	} else if (track->GetShape() == I3Particle::ContainedTrack) {
-		entry_t = track->GetTime();
-		exit_t = track->GetTime() +
-		    track->GetLength()/track->GetSpeed();
-	} else if (slantstart >= 0 && slantstop > slantstart) {
-		// Track equation : r = r0 + v*(t-t0)
-		// For slantstart, (zIceTop - z) = slantstart* cos(zenith),
-		// thus we know entry_t : -zspeed, because this speed was
-		// pointing in the opposite direction
-		entry_t = track->GetTime() +
-		    (I3Constants::zIceTop -
-		     slantstart*cos(track->GetZenith()/I3Units::radian) 
-		     - track->GetZ())/-zspeed;  
-		exit_t  = track->GetTime() + 
-		    (I3Constants::zIceTop -
-		     slantstop * cos(track->GetZenith()/I3Units::radian)  
-		     - track->GetZ())/-zspeed;
-	} else {
-		// Shift the entry point to be an integer number of the largest 
-		// segment spacing from the vertex. This simplifies gradient
-		// calculation, since it makes x - x0 fixed under small
-		// displacements.
-		double seg_duration = ((muonspacing > showerspacing) ?
-		    muonspacing : showerspacing)/track->GetSpeed();
-		entry_t = round((entry_t - track->GetTime())/seg_duration)*
-		    seg_duration + track->GetTime();
-	}
-	
-	if (showerspacing > 0) for (double t = entry_t; t < exit_t;
-	    t += showerspacing/track->GetSpeed()) {
-		I3Particle shower;
-		shower.SetDir(track->GetDir());
-		shower.SetLength(showerspacing);
-		shower.SetType(track->GetType());
-		shower.SetSpeed(track->GetSpeed());
-		shower.SetShape(I3Particle::Cascade);
-		shower.SetTime(t);
-		shower.SetPos(track->GetX() - xspeed*(t - track->GetTime()),
-			      track->GetY() - yspeed*(t - track->GetTime()),
-			      track->GetZ() - zspeed*(t - track->GetTime()));
-		hypothesis.push_back(shower);
-	}
-
-	if (muonspacing > 0) for (double t = entry_t; t < exit_t;
-	    t += muonspacing/track->GetSpeed()) {
-		I3Particle muon;
-		muon.SetDir(track->GetDir());
-		muon.SetLength(muonspacing);
-		muon.SetType(track->GetType());
-		muon.SetShape(I3Particle::ContainedTrack);
-		muon.SetSpeed(track->GetSpeed());
-		muon.SetTime(t);
-		muon.SetPos(track->GetX() - xspeed*(t - track->GetTime()),
-			    track->GetY() - yspeed*(t - track->GetTime()),
-			    track->GetZ() - zspeed*(t - track->GetTime()));
-		hypothesis.push_back(muon);
-	}
+	I3Particle muon, cascade;
+	muon.SetType(I3Particle::MuMinus);
+	muon.SetShape(I3Particle::ContainedTrack);
+	muon.SetLength(track->GetLength());
+	muon.SetDir(track->GetDir());
+	muon.SetPos(track->GetX(),track->GetY(),track->GetZ());
+	muon.SetTime(track->GetTime());
+	muon.SetSpeed(track->GetSpeed());
+	cascade.SetType(I3Particle::Hadrons);
+	cascade.SetShape(I3Particle::Cascade);
+	cascade.SetDir(track->GetDir());
+	cascade.SetPos(track->GetX(),track->GetY(),track->GetZ());
+	cascade.SetTime(track->GetTime());
+	cascade.SetSpeed(track->GetSpeed());
+	//cascade.SetEnergy(track->??
+	hypothesis.push_back(cascade);
+	hypothesis.push_back(muon);
 }
 
