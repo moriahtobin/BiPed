3a6a5d8d94bff4d0e46f077cf0cdc7615ec0a84b
moriah.tobin <moriah.tobin@16731396-06f5-0310-8873-f7f720988828>
2015-02-27 00:46:39 +0000

    working on refix for zen/azi cleaned up

--- private/LikBiped.cxx
+++ private/LikBiped.cxx
@@ -454,7 +454,7 @@ BipedLikelihood::GetLogLikelihood(const I3EventHypothesis &hypo,
 	//includes lever arm effect
         std::vector<double> muonLengths((*microSources).size(), 0.);
                 for (unsigned i = 1; i <=(*microSources).size(); i++)
-                muonLengths[i] = scale*(sources[i].GetPos() - sources[0].GetPos()).Magnitude();
+                muonLengths[i] = ((*microSources)[i].GetPos() - (*microSources)[0].GetPos()).Magnitude();
 	double zen((*microSources)[1].GetZenith()), azi((*microSources)[1].GetAzimuth());
 	for (unsigned i = 11; i < grad_trip->nrow-1; i+=7) {
 		unsigned nom=1;
