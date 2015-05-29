1b99e093e80a479539b04a8872365e99c9c64e19
moriah.tobin <moriah.tobin@16731396-06f5-0310-8873-f7f720988828>
2015-05-29 21:05:06 +0000

    Seed Chooser works again

--- resources/examples/biped-mono-2seed.py
+++ resources/examples/biped-mono-2seed.py
@@ -34,36 +34,45 @@ print "Yum Photonics Tables!"
 
 #Begin:
 tray = I3Tray()
-icetray.set_log_level(icetray.I3LogLevel.LOG_TRACE)
+#icetray.set_log_level(icetray.I3LogLevel.LOG_TRACE)
 #icetray.set_log_level_for_unit('lilliput',icetray.I3LogLevel.LOG_TRACE)
 #icetray.set_log_level_for_unit('I3Gulliver',icetray.I3LogLevel.LOG_TRACE)
 #icetray.set_log_level_for_unit('millipede',icetray.I3LogLevel.LOG_TRACE)
 tray.AddModule('I3Reader', 'reader', FilenameList=files)
 
-
+#def copytimerange(frame):
+#        if frame.Has('SRTTWOfflinePulsesDC'):
+#                nom=frame['SRTTWOfflinePulsesDCTimeRange']
+#                frame.Put('OfflinePulsesSRTTimeRange', nom)
+#tray.AddModule(copytimerange, "CopyTimeRanger")
 def copytimerange(frame):
 	if frame.Has('OfflinePulses_sRT'):
 		nom=frame['OfflinePulses_TWTimeRange']
 		frame.Put('OfflinePulses_sRTTimeRange', nom)
 tray.AddModule(copytimerange, "CopyTimeRanger")
 
-
-
 #Seed creating Module
-def Hybridforge(frame, seed, seedT, factor, output):
+def Hybridforge(frame, seed, seedTime, factor, output): #seedC, seedT, output):
     if frame.Has(seed):
         source = frame[seed]
-	sourceT = frame[seedT]
-	cogPos = common_variables.hit_statistics.calculate_cog(geometry_,frame["String1Pulses"].apply(frame))
-	qtot = common_variables.hit_statistics.calculate_q_tot_pulses(geometry_,frame["OfflinePulses_sRT"].apply(frame))
+	sourceTime = frame[seedTime]
+#	sourceT = frame[seedT]
+#	sourceC = frame[seedC]
+	if frame.Has("String1Pulses"):
+		cogPos = common_variables.hit_statistics.calculate_cog(geometry_,frame["String1Pulses"].apply(frame))
+	else:
+		cogPos = frame["FirstHLCvertex"].pos
+	qtot = common_variables.hit_statistics.calculate_q_tot_pulses(geometry_,frame['OfflinePulses_sRT'].apply(frame)) #"SRTTWOfflinePulsesDC"
         forged_particle = dataclasses.I3Particle()
-        forged_particle.energy = qtot.value
+#        forged_particle.energy = sourceT.energy + sourceC.energy
+	forged_particle.energy = qtot.value
+#	forged_particle.length = sourceT.length
 	forged_particle.length = factor*qtot.value
         forged_particle.dir = source.dir
         forged_particle.pos.x = cogPos.x+2
         forged_particle.pos.y = cogPos.y+2
         forged_particle.pos.z = cogPos.z-2
-        forged_particle.time = sourceT.time
+        forged_particle.time = sourceTime.time
         forged_particle.speed = 0.29979245799999998
         forged_particle.shape = dataclasses.I3Particle.ParticleShape.ContainedTrack
         forged_particle.fit_status = dataclasses.I3Particle.FitStatus.OK
@@ -104,8 +113,10 @@ tray.AddModule('I3ParticleForgeModule', 'most_energetic_primary',
 
 #Seeds with most common muon/cascade energy sharing patterns (most energy in muon vs. equal sharing between muon and cascade)
 #llh does not change for length minimization with StepL < MuonSpacing, so we want our seed to be as close to the actual value as possible
-tray.AddModule(Hybridforge,seed='LineFit_DC',seedT='CascadeLast_DC', factor=4.5, output='LongMuon')
-tray.AddModule(Hybridforge,seed='LineFit_DC',seedT='CascadeLast_DC',factor=2.3, output='EqualMuon')
+tray.AddModule(Hybridforge,seed='LineFit_DC',seedTime='CascadeLast_DC', factor=4.5, output='LongMuon')
+tray.AddModule(Hybridforge,seed='LineFit_DC',seedTime='CascadeLast_DC',factor=2.3, output='EqualMuon')
+#tray.AddModule(Hybridforge,seed='MPEFit',seedTime='CascadeLast_DC',seedT='SANTA_Muon',seedC='SANTA_Cascade', output='LongMuon')
+#tray.AddModule(Hybridforge,seed='MPEFit',seedTime='CascadeLast_DC',seedT='SANTA_Muon',seedC='SANTA_Cascade', factor=2.3, output='EqualMuon')
 #tray.AddModule(Hybridforge,seed='MonopodFit',seedA='MPEFitEuler_Contained',factor=4.5,output='LongMuon')
 #tray.AddModule(Hybridforge,seed='MonopodFit',seedA='MPEFitEuler_Contained',factor=2.3,output='EqualMuon')
 #tray.AddModule(Hybridforge,seed='Monopod',factor=4.5,output='LongMuon')
@@ -179,18 +190,18 @@ tray.AddService('BipedParametrizationFactory', 'bipedparam',
 #Likelihood:
 tray.AddService('BipedLikelihoodFactory', 'bipedllh',
     MuonPhotonicsService=muon_service, CascadePhotonicsService=cascade_service_mie,
-    PhotonsPerBin=5, MuonSpacing=3, Pulses='OfflinePulses_sRT')
+    PhotonsPerBin=5, MuonSpacing=3, Pulses='OfflinePulses_sRT') #'SRTTWOfflinePulsesDC')
 
 
 
 #Minimizers:
 tray.AddService('I3GSLRandomServiceFactory','I3RandomService')
 tray.AddService('I3GulliverMinuit2Factory', 'NoEDM',
-    MaxIterations=1, 
+    MaxIterations=1000, 
     Algorithm='MIGRAD', MinuitStrategy=0,
     WithGradients=True,
-    CheckGradient=False,
     IgnoreEDM=True,
+    FlatnessCheck=True
     )
 tray.AddService('I3GulliverMinuit2Factory', 'minuitM0',
     MaxIterations=3000, Algorithm='MIGRAD', MinuitStrategy=0, 
@@ -226,36 +237,39 @@ tray.AddService('I3GulliverLBFGSBFactory', 'JakobMagic',
 tray.AddService('I3BasicSeedServiceFactory', 'long', 
     FirstGuess='LongMuon',
     TimeShiftType='TNone')
-#tray.AddService('I3BasicSeedServiceFactory', 'equal', 
-#    FirstGuess='EqualMuon',
-#    TimeShiftType='TNone')
+tray.AddService('I3BasicSeedServiceFactory', 'equal', 
+    FirstGuess='EqualMuon',
+    TimeShiftType='TNone')
 #tray.Add('I3LogLikelihoodCalculator', 'LongMuonFitParams',
 #    FitName='LongMuon', LogLikelihoodService='bipedllh')
 #tray.Add('I3LogLikelihoodCalculator', 'EqualMuonFitParams',
 #    FitName='EqualMuon', LogLikelihoodService='bipedllh')
-#tray.AddModule('I3SimpleFitter', 'Long', SeedService='long',
-#    Parametrization='static',LogLikelihood='bipedllh',
-#    Minimizer='NoEDM')
-#tray.AddModule('I3SimpleFitter', 'Equal', SeedService='equal',
-#    Parametrization='static',LogLikelihood='bipedllh',
-#    Minimizer='NoEDM')
+tray.AddModule('I3SimpleFitter', 'Long', SeedService='long',
+    Parametrization='static',LogLikelihood='bipedllh',
+    Minimizer='NoEDM')
+tray.AddModule('I3SimpleFitter', 'Equal', SeedService='equal',
+    Parametrization='static',LogLikelihood='bipedllh',
+    Minimizer='NoEDM')
+
+
 
 
 
 
 #Find best seed hypothesis:
-#bestseed='BestSeed'
-#tray.AddModule(BiPedChooser, 'CoinToss', winner=bestseed, seedlist=['Long', 'Equal'])
-#tray.AddService('I3BasicSeedServiceFactory', 'seed', 
-#    FirstGuess=bestseed,
-#    TimeShiftType='TNone')
+bestseed='BestSeed'
+tray.AddModule(BiPedChooser, 'CoinToss', winner=bestseed, seedlist=['Long', 'Equal'])
+tray.AddService('I3BasicSeedServiceFactory', 'seed', 
+    FirstGuess=bestseed,
+    TimeShiftType='TNone')
+
 
 
 
 
 
 #Fit with best seed:
-tray.AddModule('I3SimpleFitter', 'BiPedHardCodeFit', SeedService='long', #'seed',
+tray.AddModule('I3SimpleFitter', 'BiPedHardCodeFit', SeedService='seed',
     Parametrization='bipedparam', LogLikelihood='bipedllh',
     Minimizer=sys.argv[1])
 
